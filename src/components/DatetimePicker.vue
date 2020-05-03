d9f187289774bdf9dfd822762a5a89de04fe03df
SeagleDub <43251625+SeagleDub@users.noreply.github.com>
2020-05-04 00:33:47 +0300

    Update DatetimePicker.vue
    added max and min limits

--- src/components/DatetimePicker.vue
+++ src/components/DatetimePicker.vue
@@ -1,241 +1,262 @@
 <template>
-  <v-dialog v-model="display" :width="dialogWidth">
-    <template v-slot:activator="{ on }">
-      <v-text-field
-        v-bind="textFieldProps"
-        :disabled="disabled"
-        :loading="loading"
-        :label="label"
-        :value="formattedDatetime"
-        v-on="on"
-        readonly
-      >
-        <template v-slot:progress>
-          <slot name="progress">
-            <v-progress-linear color="primary" indeterminate absolute height="2"></v-progress-linear>
-          </slot>
+    <v-dialog v-model="display" :width="dialogWidth">
+        <template v-slot:activator="{ on }">
+            <v-text-field
+                v-bind="textFieldProps"
+                :disabled="disabled"
+                :loading="loading"
+                :label="label"
+                :value="formattedDatetime"
+                v-on="on"
+                readonly
+            >
+                <template v-slot:progress>
+                    <slot name="progress">
+                        <v-progress-linear color="primary" indeterminate absolute height="2"></v-progress-linear>
+                    </slot>
+                </template>
+            </v-text-field>
         </template>
-      </v-text-field>
-    </template>
 
-    <v-card>
-      <v-card-text class="px-0 py-0">
-        <v-tabs fixed-tabs v-model="activeTab">
-          <v-tab key="calendar">
-            <slot name="dateIcon">
-              <v-icon>event</v-icon>
-            </slot>
-          </v-tab>
-          <v-tab key="timer" :disabled="dateSelected">
-            <slot name="timeIcon">
-              <v-icon>access_time</v-icon>
-            </slot>
-          </v-tab>
-          <v-tab-item key="calendar">
-            <v-date-picker v-model="date" v-bind="datePickerProps" :min="minDate" :max="maxDate" @input="showTimePicker" full-width></v-date-picker>
-          </v-tab-item>
-          <v-tab-item key="timer">
-            <v-time-picker
-              ref="timer"
-              class="v-time-picker-custom"
-              v-model="time"
-              :min="minTime"
-              :max="maxTime"
-              v-bind="timePickerProps"
-              full-width
-            ></v-time-picker>
-          </v-tab-item>
-        </v-tabs>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <slot name="actions" :parent="this">
-          <v-btn color="grey lighten-1" text @click.native="clearHandler">{{ clearText }}</v-btn>
-          <v-btn color="green darken-1" text @click="okHandler">{{ okText }}</v-btn>
-        </slot>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+        <v-card>
+            <v-card-text class="px-0 py-0">
+                <v-tabs fixed-tabs v-model="activeTab">
+                    <v-tab key="calendar">
+                        <slot name="dateIcon">
+                            <v-icon>event</v-icon>
+                        </slot>
+                    </v-tab>
+                    <v-tab key="timer" :disabled="dateSelected">
+                        <slot name="timeIcon">
+                            <v-icon>access_time</v-icon>
+                        </slot>
+                    </v-tab>
+                    <v-tab-item key="calendar">
+                        <v-date-picker
+                            v-model="date"
+                            v-bind="datePickerProps"
+                            :min="minDate"
+                            :max="maxDate"
+                            @input="showTimePicker"
+                            full-width
+                        />
+                    </v-tab-item>
+                    <v-tab-item key="timer">
+                        <v-time-picker
+                            ref="timer"
+                            class="v-time-picker-custom"
+                            v-model="time"
+                            v-bind="timePickerProps"
+                            :min="minTime"
+                            :max="maxTime"
+                            full-width
+                        />
+                    </v-tab-item>
+                </v-tabs>
+            </v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <slot name="actions" :parent="this">
+                    <v-btn color="grey lighten-1" text @click.native="clearHandler">{{ clearText }}</v-btn>
+                    <v-btn color="green darken-1" text @click="okHandler">{{ okText }}</v-btn>
+                </slot>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </template>
 
 <script>
-import { format, parse } from 'date-fns'
+    import {format, parse} from 'date-fns'
 
-const DEFAULT_DATE = ''
-const DEFAULT_TIME = '00:00:00'
-const DEFAULT_DATE_FORMAT = 'yyyy-MM-dd'
-const DEFAULT_TIME_FORMAT = 'HH:mm:ss'
-const DEFAULT_DIALOG_WIDTH = 340
-const DEFAULT_CLEAR_TEXT = 'CLEAR'
-const DEFAULT_OK_TEXT = 'OK'
+    const DEFAULT_DATE = ''
+    const DEFAULT_TIME = '00:00:00'
+    const DEFAULT_DATE_FORMAT = 'yyyy-MM-dd'
+    const DEFAULT_TIME_FORMAT = 'HH:mm:ss'
+    const DEFAULT_DIALOG_WIDTH = 340
+    const DEFAULT_CLEAR_TEXT = 'CLEAR'
+    const DEFAULT_OK_TEXT = 'OK'
 
-export default {
-  name: 'v-datetime-picker',
-  model: {
-    prop: 'datetime',
-    event: 'input'
-  },
-  props: {
-    datetime: {
-      type: [Date, String],
-      default: null
-    },
-    disabled: {
-      type: Boolean
-    },
-    loading: {
-      type: Boolean
-    },
-    label: {
-      type: String,
-      default: ''
-    },
-    dialogWidth: {
-      type: Number,
-      default: DEFAULT_DIALOG_WIDTH
-    },
-    dateFormat: {
-      type: String,
-      default: DEFAULT_DATE_FORMAT
-    },
-    timeFormat: {
-      type: String,
-      default: 'HH:mm'
-    },
-    clearText: {
-      type: String,
-      default: DEFAULT_CLEAR_TEXT
-    },
-    okText: {
-      type: String,
-      default: DEFAULT_OK_TEXT
-    },
-    textFieldProps: {
-      type: Object
-    },
-    datePickerProps: {
-      type: Object
-    },
-    timePickerProps: {
-      type: Object
-    },
-    datetimeMax: {
-      type: [Date],
-      default: null,
-    },
-    datetimeMin: {
-      type: [Date],
-      default: null,
-    },
-  },
-  data() {
-    return {
-      display: false,
-      activeTab: 0,
-      date: DEFAULT_DATE,
-      time: DEFAULT_TIME
-    }
-  },
-  mounted() {
-    this.init()
-  },
-  computed: {
-    maxDate() {
-      return this.datetimeMax
-        ? this.datetimeMax.toISOString().substr(0, 10)
-        : null;
-    },
-    maxTime() {
-      if (!this.datetimeMax || !this.date) 
-        return null;
-      let eqDay =
-        new Date(this.date).toISOString().substr(0, 10) ==
-        this.datetimeMax.toISOString().substring(0, 10); 
-      return eqDay 
-        ? this.datetimeMax.toLocaleTimeString() 
-        : null;
-    },
-    minDate() {
-      return this.datetimeMin
-        ? this.datetimeMin.toISOString().substr(0, 10)
-        : null;
-    },
-    minTime() {
-      if (!this.datetimeMin || !this.date) 
-        return null;
-      let eqDay =
-        new Date(this.date).toISOString().substr(0, 10) ==
-        this.datetimeMin.toISOString().substring(0, 10); 
-      return eqDay 
-        ? this.datetimeMin.toLocaleTimeString() 
-        : null;
-    },
-    dateTimeFormat() {
-      return this.dateFormat + ' ' + this.timeFormat
-    },
-    defaultDateTimeFormat() {
-      return DEFAULT_DATE_FORMAT + ' ' + DEFAULT_TIME_FORMAT
-    },
-    formattedDatetime() {
-      return this.selectedDatetime ? format(this.selectedDatetime, this.dateTimeFormat) : ''
-    },
-    selectedDatetime() {
-      if (this.date && this.time) {
-        let datetimeString = this.date + ' ' + this.time
-        if (this.time.length === 5) {
-          datetimeString += ':00'
+    export default {
+        name: 'v-datetime-picker',
+        model: {
+            prop: 'datetime',
+            event: 'input'
+        },
+        props: {
+            datetime: {
+                type: [Date, String],
+                default: null
+            },
+            disabled: {
+                type: Boolean
+            },
+            loading: {
+                type: Boolean
+            },
+            label: {
+                type: String,
+                default: ''
+            },
+            dialogWidth: {
+                type: Number,
+                default: DEFAULT_DIALOG_WIDTH
+            },
+            dateFormat: {
+                type: String,
+                default: DEFAULT_DATE_FORMAT
+            },
+            timeFormat: {
+                type: String,
+                default: 'HH:mm'
+            },
+            clearText: {
+                type: String,
+                default: DEFAULT_CLEAR_TEXT
+            },
+            okText: {
+                type: String,
+                default: DEFAULT_OK_TEXT
+            },
+            textFieldProps: {
+                type: Object
+            },
+            datePickerProps: {
+                type: Object
+            },
+            timePickerProps: {
+                type: Object
+            },
+            datetimeMax: {
+                type: [Date, String],
+                default: null,
+            },
+            datetimeMin: {
+                type: [Date, String],
+                default: null,
+            },
+        },
+        data() {
+            return {
+                display: false,
+                activeTab: 0,
+                date: DEFAULT_DATE,
+                time: DEFAULT_TIME
+            }
+        },
+        mounted() {
+            this.init()
+        },
+        computed: {
+            dtMax() {
+                if(!!this.datetimeMax) {
+                    return (this.datetimeMax instanceof Date) ? this.datetimeMax : new Date(this.datetimeMax);
+                }
+                return null;
+            },
+            dtMin() {
+                if(!!this.datetimeMin){
+                    return (this.datetimeMin instanceof Date) ? this.datetimeMin : new Date(this.datetimeMin);
+                }
+                return null;
+            },
+            maxDate() {
+                if (!!this.dtMax) {
+                    return this.dtMax.toISOString().substr(0, 10);
+                }
+                return null;
+            },
+            minDate() {
+                if (!!this.dtMin) {
+                    return this.dtMin.toISOString().substr(0, 10);
+                }
+                return null;
+            },
+            maxTime() {
+                if (!this.dtMax || !this.date)
+                    return null;
+                let eqDay =
+                    new Date(this.date).toISOString().substr(0, 10) ===
+                    this.dtMax.toISOString().substr(0, 10);
+                return eqDay
+                    ? this.dtMax.toLocaleTimeString()
+                    : null;
+            },
+            minTime() {
+                if (!this.dtMin || !this.date)
+                    return null;
+                let eqDay =
+                    new Date(this.date).toISOString().substr(0, 10) ===
+                    this.dtMin.toISOString().substr(0, 10);
+                return eqDay
+                    ? this.dtMin.toLocaleTimeString()
+                    : null;
+            },
+            dateTimeFormat() {
+                return this.dateFormat + ' ' + this.timeFormat
+            },
+            defaultDateTimeFormat() {
+                return DEFAULT_DATE_FORMAT + ' ' + DEFAULT_TIME_FORMAT
+            },
+            formattedDatetime() {
+                return this.selectedDatetime ? format(this.selectedDatetime, this.dateTimeFormat) : ''
+            },
+            selectedDatetime() {
+                if (this.date && this.time) {
+                    let datetimeString = this.date + ' ' + this.time
+                    if (this.time.length === 5) {
+                        datetimeString += ':00'
+                    }
+                    return parse(datetimeString, this.defaultDateTimeFormat, new Date())
+                } else {
+                    return null
+                }
+            },
+            dateSelected() {
+                return !this.date
+            }
+        },
+        methods: {
+            init() {
+                if (!this.datetime) {
+                    return
+                }
+
+                let initDateTime
+                if (this.datetime instanceof Date) {
+                    initDateTime = this.datetime
+                } else if (typeof this.datetime === 'string' || this.datetime instanceof String) {
+                    // see https://stackoverflow.com/a/9436948
+                    initDateTime = parse(this.datetime, this.dateTimeFormat, new Date())
+                }
+
+                this.date = format(initDateTime, DEFAULT_DATE_FORMAT)
+                this.time = format(initDateTime, DEFAULT_TIME_FORMAT)
+            },
+            okHandler() {
+                this.resetPicker()
+                this.$emit('input', this.selectedDatetime)
+            },
+            clearHandler() {
+                this.resetPicker()
+                this.date = DEFAULT_DATE
+                this.time = DEFAULT_TIME
+                this.$emit('input', null)
+            },
+            resetPicker() {
+                this.display = false
+                this.activeTab = 0
+                if (this.$refs.timer) {
+                    this.$refs.timer.selectingHour = true
+                }
+            },
+            showTimePicker() {
+                this.activeTab = 1
+            }
+        },
+        watch: {
+            datetime: function () {
+                this.init()
+            }
         }
-        return parse(datetimeString, this.defaultDateTimeFormat, new Date())
-      } else {
-        return null
-      }
-    },
-    dateSelected() {
-      return !this.date
     }
-  },
-  methods: {
-    init() {
-      if (!this.datetime) {
-        return
-      }
-
-      let initDateTime
-      if (this.datetime instanceof Date) {
-        initDateTime = this.datetime
-      } else if (typeof this.datetime === 'string' || this.datetime instanceof String) {
-        // see https://stackoverflow.com/a/9436948
-        initDateTime = parse(this.datetime, this.dateTimeFormat, new Date())
-      }
-
-      this.date = format(initDateTime, DEFAULT_DATE_FORMAT)
-      this.time = format(initDateTime, DEFAULT_TIME_FORMAT)
-    },
-    okHandler() {
-      this.resetPicker()
-      this.$emit('input', this.selectedDatetime)
-    },
-    clearHandler() {
-      this.resetPicker()
-      this.date = DEFAULT_DATE
-      this.time = DEFAULT_TIME
-      this.$emit('input', null)
-    },
-    resetPicker() {
-      this.display = false
-      this.activeTab = 0
-      if (this.$refs.timer) {
-        this.$refs.timer.selectingHour = true
-      }
-    },
-    showTimePicker() {
-      this.activeTab = 1
-    }
-  },
-  watch: {
-    datetime: function() {
-      this.init()
-    }
-  }
-}
 </script>
